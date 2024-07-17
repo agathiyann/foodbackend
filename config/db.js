d668fe9dabd34c8effa5676cb3010b9149790161
agathiyann <74144574+agathiyann@users.noreply.github.com>
2024-07-17 11:32:12 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/food', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect('mongodb+srv://agathiyan:foodmenu1234@foodmenu.bdyhk7b.mongodb.net/foodmenu');
         console.log('MongoDB connected');
     } catch (error) {
         console.error(`Error: ${error.message}`);
